69fd79940cfdd9e48ca5507d0681a18e6e16821c
Rupesh Pratap Singh <133653197+rupesh72380singh@users.noreply.github.com>
2024-08-25 02:29:07 +0530

    adding script in heriku

---
+++ app.js
@@ -0,0 +1,23 @@
+const express =require("express");
+const path = require("path")
+// const fs = require('fs')
+const app = express();
+const port= process.env.Port || 8000;
+
+app.use('/static',express.static('static'))
+app.use(express.urlencoded())
+
+app.set('view engine','pug');
+app.set('views', path.join(__dirname,'views'))
+
+
+
+app.get('/',(req,res)=>{
+    const params = { }
+    res.status(200).render('index.pug',params);
+    
+})
+
+app.listen(port, ()=>{
+    console.log(`the application startted succesfully on port ${port}`)
+});
